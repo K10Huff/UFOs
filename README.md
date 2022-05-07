571b4958009be0aac9715960eadf74adc0067565
unknown <kris10huffman@gmail.com>
2022-05-07 04:47:24 -0700

    Deliverables & ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # UFOs
-Dynamic and interactive website build using JavaScript and HTML
+
+# Overview of the Analysis
+The goal this project was to provide an interactive and dynamic website, using JavaScript and HTML, that allowed users to filter a dataset of UFO sightings to search by date, city, state, country, and shape.
+
+# Results
+To perform a search, the user must enter in their search criteria into the forms on the left side of the website and press enter after each criterion has been entered. Their options include searching by date, city, state, country, or shape. If the user would like to reset the page the need to click on the "UFO Sightings" button up on the Navigation bar at the top of the web page. <br>
+
+![Example Search Image 01](link) <br>
+![ Example Search Image 02](link)
+
+# Summary
+One drawback of this website is that it does not provide the user with what their search options are, so a drop-down menu might be of some assistance. Another feature that could be useful would be a "Search" button the user could click to make it more apparent how to start the search query. Lastly, the website does not provide any summary statics regarding the number of sightings by date, city, state, country, or shape. Further development of this site could include providing the user with such summaries and including a drop-down menu and the Search button. An additional development could be mapping the sightings onto an interactive map.
---
+++ static/js/app_mod.js
@@ -0,0 +1,52 @@
+// 1. import the data from data.js
+const tableData = data;
+
+// 2. Point our data to our HTML page
+//tell JavaScript what type of element the data will be displayed in
+// data will be placed in a table- reference the tbody HTML tag using D3.
+// D3 is a JavaScript library that produces sophisticated and highly dynamic graphics in an HTML webpage.
+var tbody = d3.select("tbody");
+
+// 3. Build the table to display all of the UFO sightings.
+// Iterate through the array of objects in our data file and then append them to a table row.
+// Clearing existing data is important because we’ll be appending data from the array in the following code. 
+// Without clearing it, the users will end up with pre-filtered data.
+function buildTable(data) {
+
+    tbody.html("");
+
+// 4. Create another function specifically for building the table.
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");
+
+ // Loop through each field in the dataRow and add each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+        cell.text(val);
+      }
+    );
+  });
+}
+
+// 5. Add the ability to filter the data.
+function handleClick() {
+
+  // Grab the datetime value from the filter
+  let date = d3.select("#datetime").property("value");
+  let filteredData = tableData;
+
+ // 6. Apply a filter method that will match the datetime value to the filtered date value
+    filteredData = filteredData.filter(row => row.datetime === date);
+  }
+
+// 7. Build the Filtered Table
+//  Pass in filteredData variable as the argument so that only the data that matches the filter is displayed..
+  buildTable(filteredData);
+}
+
+// Listen for the Event
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+/// Build the Final Table - OG table loads as soon as the page does.
+buildTable(tableData);
